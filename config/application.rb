b92363520b30ff5c8e12a2bb7b6805b32f083de6
nakao11 <pizaya_1@yahoo.com>
2024-11-07 14:15:01 +0900

    #1 FactoryBot導入、spring導入

--- config/application.rb
+++ config/application.rb
@@ -20,20 +20,18 @@ Bundler.require(*Rails.groups)
 
 module ShiftApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false # CSS, JavaScriptファイルを自動生成しない
+      g.helper false # helperファイルを自動生成しない
+      g.test_framework :rspec,
+        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+        view_specs: false, # ビューファイル用のスペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb用のスペックファイル作成しない
+    end
   end
 end
