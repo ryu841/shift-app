4a1780f0569289b29eda2d7baf9bada724edca9e
nakao11 <pizaya_1@yahoo.com>
2024-11-08 16:29:01 +0900

    #3 Userモデルにfirstnameとlastnameカラムを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    firstname { 'テスト' }
+    lastname { '太郎' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '111111' }
+    password_confirmation { '111111' }
   end
 end
