8598687069bef558b7f71ad025a9c9d03382408b
nakao11 <pizaya_1@yahoo.com>
2024-11-08 18:31:48 +0900

    #3 firstname,lastnameの文字数のバリデーションを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   let(:firstname) { 'テスト' }
   let(:lastname) { '太郎' }
   let(:email) { 'test@example.com' }
-  let(:user) { create(:user, firstname: firstname, lastname: lastname, email: email) }
+  let(:user) { build(:user, firstname: firstname, lastname: lastname, email: email) }
 
   describe '.first' do
     subject { user }
@@ -13,6 +13,34 @@ RSpec.describe User, type: :model do
       expect(subject.firstname).to eq(firstname)
       expect(subject.lastname).to eq(lastname)
       expect(subject.email).to eq(email)
+    end
+  end
+
+  describe 'validation' do
+    describe 'firstname,lastname属性' do
+      describe '文字数制限の検証' do
+        context '文字数が10文字以下の場合' do
+          let(:firstname) { 'あいうえおかきくけこ' } #10文字
+          let(:lastname) { 'あいうえおかきくけこ' } #10文字
+          
+          it 'Userオブジェクトは有効である' do
+            expect(user.valid?).to be(true)
+          end
+        end
+
+        context '文字数が10文字を超える場合' do
+          let(:firstname) { 'あいうえおかきくけこさ' } #11文字
+          let(:lastname) { 'あいうえおかきくけこさ' } #11文字
+
+          it 'Userオブジェクトは無効である' do
+            user.valid?
+
+            expect(user.valid?).to be(false)
+            expect(user.errors[:firstname]).to include('is too long (maximum is 10 characters)')
+            expect(user.errors[:lastname]).to include('is too long (maximum is 10 characters)')
+          end
+        end
+      end
     end
   end
 end
