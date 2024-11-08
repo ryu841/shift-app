b425aec0693d82a22e0f6d7b2c2c106b63a6a049
nakao11 <pizaya_1@yahoo.com>
2024-11-08 16:05:44 +0900

    #3 Userモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
