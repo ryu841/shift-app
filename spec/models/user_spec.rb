310804dc3c4e0d59732917e444fbf2d7fa928b94
nakao11 <pizaya_1@yahoo.com>
2024-11-08 16:41:30 +0900

    #3 FactoryBotのModel spec追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  let(:firstname) { 'テスト' }
+  let(:lastname) { '太郎' }
+  let(:email) { 'test@example.com' }
+  let(:user) { create(:user, firstname: firstname, lastname: lastname, email: email) }
+
+  describe '.first' do
+    subject { user }
+
+    it '事前に作成した通りのUserを返す' do
+      expect(subject.firstname).to eq(firstname)
+      expect(subject.lastname).to eq(lastname)
+      expect(subject.email).to eq(email)
+    end
+  end
 end
