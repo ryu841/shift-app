b693350f8a1501f7dbda1979a152fdad3d6b8a13
nakao11 <pizaya_1@yahoo.com>
2024-11-11 11:42:14 +0900

    #7 Shiftsコントローラーにnewとcreateアクション追加

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,4 +1,25 @@
 class ShiftsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @shift = Shift.new
   end
+
+  def create
+    @shift = Shift.new(shift_params)
+    # @shift.admin_id = current_user.id
+    if @shift.save
+      flash[:notice] = I18n.t('flash.posts.create.success')
+      redirect_to root_path
+    else
+      flash[:alert] = I18n.t('flash.posts.create.failure')
+      render :new
+    end
+  end
+
+  private
+
+    def shift_params
+      params.require(:shift).permit(:title_date, :comment)
+    end
 end
