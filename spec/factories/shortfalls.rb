32adb79ada96b87a4733d0225028b1e83dd9c528
nakao11 <pizaya_1@yahoo.com>
2024-11-11 21:22:02 +0900

    #11 FactoryBot作成、Model spec作成

--- spec/factories/shortfalls.rb
+++ spec/factories/shortfalls.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :shortfall do
-    
+    start_time { '2024-11-11 09:00:00' }
+    end_time {'2024-11-11 15:00:00' }
+    require_count { 2 }
+    association :shift
   end
 end
--- spec/models/shortfall_spec.rb
+++ spec/models/shortfall_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Shortfall, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  let(:shift) { create(:shift) }
+  let(:start_time) { '2024-11-11 09:00:00' }
+  let(:end_time) {'2024-11-11 15:00:00' }
+  let(:require_count) { 2 }
+  let(:shortfall) { Shortfall.new(start_time: start_time, end_time: end_time, require_count: require_count, shift: shift) }
+
+  describe 'バリデーションの検証' do
+    context '正常系' do
+      it '有効である' do
+        expect(shortfall.valid?).to be(true)
+      end
+    end
+  end
+
+  describe 'Shortfallが持つ情報の検証' do
+    it 'Shortfallの属性値を返す' do
+      expect(shortfall.start_time).to eq('2024-11-11 09:00:00')
+      expect(shortfall.end_time).to eq('2024-11-11 15:00:00')
+      expect(shortfall.require_count).to eq(2)
+    end
+  end
+end 
