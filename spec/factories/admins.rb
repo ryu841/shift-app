fbcec7b322caca85772cbb1951bd13491baa2b73
nakao11 <pizaya_1@yahoo.com>
2024-11-13 13:18:12 +0900

    #15 FactoryBot、Model spec作成

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :admin do
-    
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '111111' }
   end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email) { 'test@example.com' }
+  let(:admin) { build(:admin, email: email) }
+
+  describe '.first' do
+    subject { admin }
+
+    it '事前に作成した通りのAdminを返す' do
+      expect(subject.email).to eq(email)
+    end
+  end
 end
