fd4bffd36c4fa82a1452d40ceae011b8cba48be1
nakao11 <pizaya_1@yahoo.com>
2024-11-18 21:20:31 +0900

    #20  承認一覧を押すと承認ページに飛び、コメントもそこに表示される

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,18 @@
 class TicketsController < ApplicationController
-  def new
+  before_action :set_ticket, only: [:approve, :reject]
+  before_action :set_shift, only: [:create, :reject, :destroy]
+
+  def index
+    @shifts = Shift.includes(:tickets).all
+    @tickets = @shifts.flat_map(&:tickets)
   end
 
+  def new
+  end
+  
   def create
-    @shift = Shift.find(params[:shift_id])
     # @ticket = @shift.tickets.build(ticket_params)
-    # @ticket.status = 'pending'
+    @ticket.status = 'pending'
     @ticket = @shift.tickets.build(ticket_params) do |t|
       t.shift = @shift
       t.comment = params[:ticket][:comment]
@@ -26,8 +33,40 @@ class TicketsController < ApplicationController
     flash[:notice] = I18n.t('flash.tickets.destroy.success')
   end
 
+  def approve
+    @ticket.update!(status: 'approved')
+    flash[:notice] = I18n.t('flash.tickets.approve.success')
+    redirect_to shift_tickets_path(@ticket.shift_id)
+  end
+
+  def reject
+    @ticket.update!(status: 'rejected')
+    flash[:notice] = I18n.t('flash.tickets.reject.success')
+    redirect_to shift_tickets_path(@ticket.shift_id)
+  end
+
+  def update
+    ticket = Ticket.find(params[:id])
+    if params[:status] == 'approved'
+      ticket.update!(status: 'approved')
+      flash[:notice] = I18n.t('flash.tickets.approve.success')
+    elsif params[:status] == 'rejected'
+      ticket.update!(status: 'rejected')
+      flash[:notice] = I18n.t('flash.tickets.reject.success')
+    end
+    redirect_to tickets_path
+  end
+
   private
   def ticket_params
     params.require(:ticket).permit(:comment, :shortfall_id, :user_id).merge(user_id: current_user.id)
+  end
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def set_shift
+    @shift = Shift.find(params[:shift_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,21 @@ Rails.application.routes.draw do
     confirmations: "admin/confirmations"
   }
   devise_for :users
+
+  root "home#top"
+
+  resources :shifts, only: [:new, :create, :show, :index, :edit, :update, :destroy] do
+    resources :tickets, only: [:create, :destroy, :show, :index] do
+      member do
+        patch :approve
+        patch :reject
+      end
+    end
+  end
+
+  resources :tickets, only: [:index]
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  root "home#top"
-  resources :shifts, only: [:new, :create, :show, :index, :edit, :update, :destroy] do
-    resources :tickets, only: [:create, :destroy]
-  end
 end
