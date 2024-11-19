bb0aa2d607d753a21ddded407bbdcc36b1544228
nakao11 <pizaya_1@yahoo.com>
2024-11-19 20:39:59 +0900

    #23 シフトが7日を経過した場合にアーカイブする処理をシフトモデルに追加

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,6 @@
 class Shift < ApplicationRecord
+  before_save :archive_old_shifts
+
   belongs_to :admin
   has_many :tickets, dependent: :destroy
   has_many :tickets_user, through: :tickets, source: :user
@@ -6,4 +8,12 @@ class Shift < ApplicationRecord
   accepts_nested_attributes_for :shortfalls, allow_destroy: true, reject_if: :all_blank, limit: 5
 
   validates :title_date, presence: true
+
+  private
+
+  def archive_old_shifts
+    return unless title_date.present? && title_date <= 7.days.ago.to_date
+
+    self.archived = true
+  end
 end
