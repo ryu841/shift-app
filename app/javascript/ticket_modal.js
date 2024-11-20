13fd55032ece8940285fed270582fb76a188cf43
nakao11 <pizaya_1@yahoo.com>
2024-11-21 04:30:49 +0900

    #26 シフト参加時のコメントを５０文字以内にする

--- app/javascript/ticket_modal.js
+++ app/javascript/ticket_modal.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const modalTicket = document.getElementById("ticket-modal");
   const openButtonTicket = document.getElementById("ticket-button");
   const closeButtonTicket = document.getElementById("modal-cancel");
+  const errorDiv = document.getElementById("comment-error");
+  const submitButton = document.getElementById("submit-button");
+  const commentField = document.getElementById("comment-field");
 
   // モーダルを開く
   openButtonTicket.addEventListener("click", () => {
@@ -17,6 +20,18 @@ document.addEventListener("DOMContentLoaded", () => {
   modalTicket.addEventListener("click", (event) => {
     if (event.target === modalTicket) {
       modalTicket.classList.add("hidden");
+    }
+  });
+
+  // 送信ボタンを押したら、コメントが50文字以内かをチェックする
+  submitButton.addEventListener("click", (event) => {
+    const commentLength = commentField.value.length;
+
+    if (commentLength > 50) {
+      event.preventDefault();
+      errorDiv.classList.remove('hidden');
+    } else {
+      errorDiv.classList.add('hidden');
     }
   });
 });
