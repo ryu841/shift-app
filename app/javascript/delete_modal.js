96ce9cc518641bddf1690451efe635d9ee0c3286
nakao11 <pizaya_1@yahoo.com>
2024-11-29 23:50:07 +0900

    #25 シフト削除のときにもモーダルを表示させるようにする

--- app/javascript/delete_modal.js
+++ app/javascript/delete_modal.js
@@ -1,28 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const openButtonDelete = document.querySelectorAll(`[id^="delete-button-"]`);
+  const openButtonDelete = document.getElementById("delete-button");
 
-  openButtonDelete.forEach(button => {
-    const index = button.id.split('-').pop();
+  //indexに対応するモーダルを取得
+  const modalDelete = document.getElementById("delete-modal");
+  const closeButtonDelete = document.getElementById("modal-cancel");
 
-    //indexに対応するモーダルを取得
-    const modalDelete = document.getElementById(`delete-modal-${index}`);
-    const closeButtonDelete = document.getElementById(`modal-cancel-${index}`);
+  // モーダルを開く
+  openButtonDelete.addEventListener("click", () => {
+    modalDelete.classList.remove("hidden");
+  });
 
-    // モーダルを開く
-    button.addEventListener("click", () => {
-      modalDelete.classList.remove("hidden");
-    });
-  
-    // モーダルを閉じる
-    closeButtonDelete.addEventListener("click", () => {
+  // モーダルを閉じる
+  closeButtonDelete.addEventListener("click", () => {
+    modalDelete.classList.add("hidden");
+  });
+
+  // モーダルの外側をクリックした場合も閉じる
+  modalDelete.addEventListener("click", (event) => {
+    if (event.target === modalDelete) {
       modalDelete.classList.add("hidden");
-    });
-  
-    // モーダルの外側をクリックした場合も閉じる
-    modalDelete.addEventListener("click", (event) => {
-      if (event.target === modalDelete) {
-        modalDelete.classList.add("hidden");
-      }
-    });
+    }
   });
 });
