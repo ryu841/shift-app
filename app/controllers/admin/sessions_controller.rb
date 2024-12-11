f5df2aa573e06f53a2b83bd57a0eda32e51a3328
nakao11 <pizaya_1@yahoo.com>
2024-12-11 09:31:25 +0900

    #5 Rubocopの指摘事項修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -4,7 +4,7 @@ class Admin::SessionsController < Devise::SessionsController
   def guest_sign_in
     admin = Admin.guest
     sign_in admin
-    flash[:notice] = "管理者ゲストユーザーとしてログインしました。"
+    flash[:notice] = I18n.t('flash.sessions.guest_admin_sign_in')
     redirect_to shifts_path
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    flash[:notice] = 'ゲストユーザーとしてログインしました。'
+    flash[:notice] = I18n.t('flash.sessions.guest_user_sign_in')
     redirect_to shifts_path
   end
 end
