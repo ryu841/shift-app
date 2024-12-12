aa1711a02c50fbb68b77641be02761e8542060a8
nakao11 <pizaya_1@yahoo.com>
2024-12-12 15:16:48 +0900

    #39 importmapの設定変更

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", preload: true
 pin_all_from "app/javascript/components", under: "components"
 pin "shortfall", to: "shortfall.js"
 pin "ticket_modal", to: "ticket_modal.js"
 pin "delete_modal", to: "delete_modal.js"
 pin "cancel_modal", to: "cancel_modal.js"
-pin "navbar_toggle", to: "navbar_toggle.js"
+pin "navbar_toggle", to: "navbar_toggle.js", preload: true
 pin "shift_form", to: "shift_form.js"
