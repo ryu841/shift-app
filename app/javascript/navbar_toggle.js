14d7b5bca5e4a0f3bedf599f2f3f0bee86eec37b
nakao11 <pizaya_1@yahoo.com>
2024-12-13 09:59:35 +0900

    #28 touchイベント修正

--- app/javascript/navbar_toggle.js
+++ app/javascript/navbar_toggle.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuToggle = document.getElementById("menu-toggle");
   const menu = document.getElementById("menu");
 
-  menuToggle.addEventListener("click", () => {
-    menu.classList.toggle("max-sm:hidden");
-  });
   menuToggle.addEventListener("touchstart", function(event) {
     event.preventDefault();
     menu.classList.toggle("max-sm:hidden");
